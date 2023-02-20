4ebbf97d8ba821660dcbddc10e83481813d89665
J0udi18 <101606075+J0udi18@users.noreply.github.com>
2023-02-21 10:55:49 +1300

    Add files via upload

--- 00_LU_Base_v_01.py
+++ 00_LU_Base_v_01.py
@@ -1,21 +1,21 @@
-
+import random
 
 # Function go here...
 def yes_no (question) :
     valid = False
     while not valid:
-         response = input(question).lower() 
+        response = input(question).lower()
 
-    if response == "yes" or response == "y":
-        response = "yes"
-        return response
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
 
-    elif response == "no" or response == "n":
-        response = "no"
-        return response
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
 
-    else:
-        print("please answer yes / no")         
+        else:
+            print("please answer yes / no")
 
 
 def instructions () :
@@ -59,4 +59,57 @@ print()
 how_much = num_check("How much would you"
                    "like to play with? ", 0, 10)
 
-print("You will be spending ${}".format(how_much))
+balance = how_much
+
+rounds_played = 0
+
+play_again = input("press <Enter> to play...").lower()
+while play_again == "":
+    # inorease # of rounds played
+    rounds_played += 1
+
+    # print round number
+    print()
+    print("*** Round #{} ***".format(rounds_played))
+
+    chosen_num = random.randint(1, 100)
+
+    # Adjust balance
+    # if the random # is between 1 and 5,
+    # users gets a unicorn (add $4 to balance)
+    if 1 <= chosen_num <= 5:
+        chosen = "unicorn"
+        balance += 4
+
+        # if the random # is between 6 and 36
+    # users gets a donkey (subtract $1 from balance)
+    elif 6 <= chosen_num <= 36:
+        chosen = "donkey"
+        balance -= 1
+    # The token is either a horse or zebra...
+    # #in both cases, subtract $0.50 from the balance
+    else:
+        # if the number is even, set the ohosen
+        # item to a horse
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+
+        # otherwise set it to a zebra
+        else:
+            chosen = "zebra"
+        balance -= 0.5
+
+    print("you got a {}. your balance is "
+          "${:.2f}".format(chosen, balance))
+
+    if balance < 1:
+        # if balance is too low, exit the game and
+        # output a suitable message
+        play_again = "xxx"
+        print("sorry you have run out of money")
+    else:
+        play_again = input("Press Enter to play again "
+                           "or 'xxx' to quit")
+
+print()
+print("final balance", balance)
